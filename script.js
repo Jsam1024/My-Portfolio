ad7d606d7f99a1a8ed7051f0cb471d2277ee50a6
Samson Adeyemo <adeyemos858@gmail.com>
2023-08-30 16:05:53 +0100

    commit

--- script.js
+++ script.js
@@ -45,3 +45,12 @@ window.onscroll = () => {
 
 
  /*----------scroll reveal-----------*/
+
+ ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.tsri', { origin: 'top' });
